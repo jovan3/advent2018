346c9d829300b774de0eaf3bb84c5ed22107a6ea
Yovan Naumovski <jovan3.14@gmail.com>
2018-12-15 13:57:04 +0200

    add day 7 part 1

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [ubergraph "0.5.2"]]
   :plugins [[cider/cider-nrepl "0.15.1"]]
   :jvm-opts ["-Xmx6g"]
   :main advent2018.core)
---
+++ src/advent2018/day7.clj
@@ -0,0 +1,38 @@
+(ns advent2018.day7
+  (:require [clojure.string :as str]
+            [clojure.set :as s]
+            [ubergraph.core :as uber]))
+
+(defn extract-graph-edges [str]
+  (let [matcher (re-matcher #"Step (\w+) must be finished before step (\w+) can begin." str)]
+    (re-find matcher)
+    (let [match-groups (re-groups matcher)
+          [_ step_before step_after] match-groups]
+      [step_before step_after])))
+
+(defn process-input [input-str]
+  (map extract-graph-edges (str/split-lines input-str)))
+
+(defn valid-successors [graph successor-nodes visited]
+  (set (filter (fn [successor-node]
+                 (every? (fn [predecessor] (contains? visited predecessor)) (uber/predecessors graph successor-node))) successor-nodes)))
+
+(defn next-nodes [graph current-node available visited]
+  (let [successor-nodes (set (uber/successors graph current-node))
+        valid-successors (valid-successors graph successor-nodes visited)]
+    (sort (s/difference (s/union valid-successors available) (set visited)))))
+  
+(defn traverse-graph [graph start-node]
+  (loop [current-node start-node
+         available #{}
+         visited #{start-node}
+         result [start-node]]
+    (let [next-nodes (next-nodes graph current-node available visited)]
+      (if (empty? next-nodes)
+        result
+        (let [next-node (first next-nodes)]
+          (recur next-node (set (remove #{next-node} next-nodes)) (conj visited next-node) (conj result next-node)))))))
+
+(defn day7 [input]
+  (let [graph (apply uber/digraph (process-input input))]
+    (println "day 7 part 1" (str/join "" (traverse-graph graph "S")))))
